8cd41c87972dd599c6634249b0f010097086dfd4
hegeduscili <hegeduscecilia@gmail.com>
2023-12-16 11:44:23 +0100

    js_mukodik_de_nem_jol

--- scripts.js
+++ scripts.js
@@ -1,2 +1,30 @@
-const images = document.getElementsByClassName('.pictures');
+const images = document.getElementsByClassName('picture');
+const fullScreenContainer = document.getElementById('full_screen');
+const greyback = document.getElementById('all');
+
+const hounds_pics = document.getElementsByClassName('hounds_pic');
+const cats_pics = document.getElementsByClassName('cats_pic');
+
+
+for (const img of images) {
+    img.addEventListener('click',function(){
+    const fullScreenImage = document.createElement('img');
+
+    fullScreenContainer.style.display = 'flex';
+
+    fullScreenImage.src = img.src;
+    fullScreenImage.alt = img.alt;
+
+    fullScreenContainer.innerHTML = '';
+    fullScreenContainer.appendChild(fullScreenImage);
+
+    fullScreenImage.style.pointerEvents = 'none';
+});
+}
+
+fullScreenContainer.addEventListener('click',function(){
+    fullScreenContainer.style.display = 'none';
+})
+
+
 
